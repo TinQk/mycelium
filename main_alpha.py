49723ac43ac8fe70279356848b662c4c2ac73e67
pierreabelroux <79593828+pierreabelroux@users.noreply.github.com>
2021-05-09 00:47:49 +0200

    Update main_alpha.py
    Alpha fonctionnelle :
     - grille avec obstacles
     - plusieurs cellules peuvent se superposer

--- main_alpha.py
+++ main_alpha.py
@@ -13,44 +13,68 @@ start_time = time.time()
 
 ## Constants
 
-GRID_LENGTH = 5
-GRID_WIDTH = 5
+GRID_LENGTH = 10
+GRID_WIDTH = 10
 
-STEPS_NUMBER=10
+STEPS_NUMBER=100
+
 
 ## Run
 
 
 grid = np.zeros((GRID_LENGTH, GRID_WIDTH))
+grid[0,0] = -1
+grid[1,0] = -1
+grid[0,1] = -1
+grid[1,1] = -1
+grid[2,0] = -1
+grid[0,2] = -1
 
-all_cells = [ [2,2] ]
 
+all_cells = [ [2,2] ,  [3,3], [7,6], [7,6], [9,9] ]
 
-
+def possible(x,y) :        
+    return not (x<0 or y<0 or x >= GRID_LENGTH or y>=GRID_WIDTH or grid[x,y] == -1)
+    
 for i in range(STEPS_NUMBER):
         
-    cell_grid = grid.copy()
+    cell_grid = np.zeros((GRID_LENGTH, GRID_WIDTH))
     for cell in all_cells :
         
-        
-        theta = rd.randint(0,3)
-        if theta == 0 :
-            cell[0] += 1
-        elif theta == 1 :
-            cell[0] -= 1
-        elif theta == 2 :
-            cell[1] += 1
-        else :
-            cell[1] -=1
-        
-        cell_grid[cell[0],cell[1]] = 100
+        b = True
+        while b :
+            b = False
+            theta = rd.randint(4)
+            if theta == 0 :
+                if possible(cell[0]+1,cell[1]) :
+                    cell[0] += 1
+                else :
+                    b = True
+            elif theta == 1 :
+                if possible(cell[0]-1,cell[1]) :
+                    cell[0] -= 1
+                else :
+                    b = True
+            elif theta == 2 :
+                if possible(cell[0],cell[1]+1) :
+                    cell[1] += 1
+                else :
+                    b = True
+            else :
+                if possible(cell[0],cell[1]-1) :
+                    cell[1] -= 1
+                else :
+                    b = True
+                
+        cell_grid[cell[0],cell[1]] += 100
         
         print("theta = ", theta)
-        print(all_cells)
-        print(grid + cell_grid)
+    
+    print(all_cells)
+    print(grid + cell_grid)
         
-        input("")
-        print ("\n" * 100) 
+    input("")
+    print ("\n" * 100) 
 
 ## Display
 

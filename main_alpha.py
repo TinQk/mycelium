0e830fc819ce62d84bc900aedd8ffa950a5753f1
Quentin Potié <potie.quentin@gmail.com>
2021-05-09 12:14:34 +0200

    Update main_alpha.py
    rename stuff and create function "main"

--- main_alpha.py
+++ main_alpha.py
@@ -7,82 +7,91 @@ import numpy.random as rd
 import matplotlib.pyplot as plt
 import time
 
-
-start_time = time.time()
-
-
 ## Constants
 
-GRID_LENGTH = 10
-GRID_WIDTH = 10
+map_LENGTH = 10
+map_WIDTH = 10
 
 STEPS_NUMBER=100
 
 
+def main():
+    start_time = time.time()
+    
+    ## Run
+    
+    # Map creation
+    map = np.zeros((map_LENGTH, map_WIDTH))
+    
+    # Obstacles
+    map[0,0] = -1
+    map[1,0] = -1
+    map[0,1] = -1
+    map[1,1] = -1
+    map[2,0] = -1
+    map[0,2] = -1
+    
+    # Cells creation
+    all_cells = [ [2,2] ,  [3,3], [7,6], [7,6], [9,9] ]
+    
+    def accessible(x,y) :        
+        return not (x<0 or y<0 or x >= map_LENGTH or y>=map_WIDTH or map[x,y] == -1)
+        
+    for i in range(STEPS_NUMBER):
+            
+        cell_map = np.zeros((map_LENGTH, map_WIDTH))
+        for cell in all_cells :
+            
+            b = True
+            while b :
+                b = False
+                theta = rd.randint(4)
+                if theta == 0 :
+                    if accessible(cell[0]+1,cell[1]) :
+                        cell[0] += 1
+                    else :
+                        b = True
+                elif theta == 1 :
+                    if accessible(cell[0]-1,cell[1]) :
+                        cell[0] -= 1
+                    else :
+                        b = True
+                elif theta == 2 :
+                    if accessible(cell[0],cell[1]+1) :
+                        cell[1] += 1
+                    else :
+                        b = True
+                else :
+                    if accessible(cell[0],cell[1]-1) :
+                        cell[1] -= 1
+                    else :
+                        b = True
+                    
+            cell_map[cell[0],cell[1]] += 100
+            
+            print("theta = ", theta)
+        
+        
+        ## Display   
+        
+        print(all_cells)
+        print(map + cell_map)
+            
+        input("")
+        print ("\n" * 100) 
+ 
+   
+    ## End
+    
+    end_time = time.time()
+    if __name__ == "__main__" :
+        print("Execution time : ",end_time-start_time)
+
+
 ## Run
 
-
-grid = np.zeros((GRID_LENGTH, GRID_WIDTH))
-grid[0,0] = -1
-grid[1,0] = -1
-grid[0,1] = -1
-grid[1,1] = -1
-grid[2,0] = -1
-grid[0,2] = -1
-
-
-all_cells = [ [2,2] ,  [3,3], [7,6], [7,6], [9,9] ]
-
-def possible(x,y) :        
-    return not (x<0 or y<0 or x >= GRID_LENGTH or y>=GRID_WIDTH or grid[x,y] == -1)
-    
-for i in range(STEPS_NUMBER):
-        
-    cell_grid = np.zeros((GRID_LENGTH, GRID_WIDTH))
-    for cell in all_cells :
-        
-        b = True
-        while b :
-            b = False
-            theta = rd.randint(4)
-            if theta == 0 :
-                if possible(cell[0]+1,cell[1]) :
-                    cell[0] += 1
-                else :
-                    b = True
-            elif theta == 1 :
-                if possible(cell[0]-1,cell[1]) :
-                    cell[0] -= 1
-                else :
-                    b = True
-            elif theta == 2 :
-                if possible(cell[0],cell[1]+1) :
-                    cell[1] += 1
-                else :
-                    b = True
-            else :
-                if possible(cell[0],cell[1]-1) :
-                    cell[1] -= 1
-                else :
-                    b = True
-                
-        cell_grid[cell[0],cell[1]] += 100
-        
-        print("theta = ", theta)
-    
-    print(all_cells)
-    print(grid + cell_grid)
-        
-    input("")
-    print ("\n" * 100) 
-
-## Display
-
-# print(grid + cell_grid)
+if __name__ == "__main__":
+    main()
 
 
 
-end_time = time.time()
-
-if __name__ == "__main__" :
-    print("Execution time : ",end_time-start_time)

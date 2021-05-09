1015003b2049c8b860884324f26a77f88776a084
pierreabelroux <79593828+pierreabelroux@users.noreply.github.com>
2021-05-09 12:40:42 +0200

    plot heatmap
    Version naïve du plot heatmap

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import numpy as np
 import numpy.random as rd
 import matplotlib.pyplot as plt
 import time
+import matplotlib.cm as cm
+import os
+
+print(os.getcwd())
 
 ## Constants
 
@@ -13,6 +17,8 @@ map_LENGTH = 10
 map_WIDTH = 10
 
 STEPS_NUMBER=100
+
+WALLS_VALUE = -100
 
 
 def main():
@@ -24,18 +30,18 @@ def main():
     map = np.zeros((map_LENGTH, map_WIDTH))
     
     # Obstacles
-    map[0,0] = -1
-    map[1,0] = -1
-    map[0,1] = -1
-    map[1,1] = -1
-    map[2,0] = -1
-    map[0,2] = -1
+    map[0,0] = WALLS_VALUE
+    map[1,0] = WALLS_VALUE
+    map[0,1] = WALLS_VALUE
+    map[1,1] = WALLS_VALUE
+    map[2,0] = WALLS_VALUE
+    map[0,2] = WALLS_VALUE
     
     # Cells creation
     all_cells = [ [2,2] ,  [3,3], [7,6], [7,6], [9,9] ]
     
     def accessible(x,y) :        
-        return not (x<0 or y<0 or x >= map_LENGTH or y>=map_WIDTH or map[x,y] == -1)
+        return not (x<0 or y<0 or x >= map_LENGTH or y>=map_WIDTH or map[x,y] == WALLS_VALUE)
         
     for i in range(STEPS_NUMBER):
             
@@ -68,17 +74,24 @@ def main():
                         b = True
                     
             cell_map[cell[0],cell[1]] += 100
-            
-            print("theta = ", theta)
-        
+          
+          
+          
         
         ## Display   
+                    
+        fig, ax = plt.subplots()
+        # ax.imshow(map + cell_map, interpolation='bilinear', cmap=cm.Greys_r)
+        ax.imshow(map + cell_map, interpolation='nearest', cmap=cm.Greys_r)
+        fig.savefig("{:04d}".format(i))
+        fig.clear()
         
-        print(all_cells)
-        print(map + cell_map)
+        
+        # print(all_cells)
+        # print(map + cell_map)
             
-        input("")
-        print ("\n" * 100) 
+        # input("")
+        # print ("\n" * 100) 
  
    
     ## End
